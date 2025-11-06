db83354fe399e684bb5008a04dbffb675132d8ab
muttakin43 <eamsiam6@gmail.com>
2025-11-06 23:37:36 +0600

    first

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,21 @@
-﻿namespace StudentManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
 {
     public class Student
     {
+
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
-         
+
+        [Required]
+        [EmailAddress]
+
         public string Email { get; set; }
+        [Required]
+
 
         public string Department { get; set; }
     }
